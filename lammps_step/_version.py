9a41c36ed11f11afa3924ec545e37d94eb3599a5
Paul Saxe <paul@thesaxes.net>
2019-09-08 20:10:37 -0400

    Fixed wrong path in _version.py.

--- lammps_step/_version.py
+++ lammps_step/_version.py
@@ -42,7 +42,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "forcefield_step/_version.py"
+    cfg.versionfile_source = "lammps_step/_version.py"
     cfg.verbose = False
     return cfg
 
