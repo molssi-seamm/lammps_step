4581c08f3bd3568292119364dcbedffc4a57ccab
Eliseo Marin <meliseo@vt.edu>
2021-04-07 14:12:24 -0400

    Remove old code that gets charges for PCFF

--- lammps_step/initialization.py
+++ lammps_step/initialization.py
@@ -127,42 +127,6 @@ class Initialization(seamm.Node):
 
         n_atoms = system_db.systems[0].configuration.n_atoms
 
-        #    # Now get the charges if forcefield has them.
-        #    terms = ff.terms
-        #    if 'bond charge increment' in terms:
-        #        logger.debug('Getting the charges for the system')
-        #        neighbors = configuration.bonded_neighbors(as_indices=True)
-
-        #        charges = []
-        #        total_q = 0.0
-        #        for i in range(configuration.n_atoms):
-        #            itype = atom_types[i]
-        #            parameters = ff.charges(itype)[3]
-        #            q = float(parameters['Q'])
-        #            for j in neighbors[i]:
-        #                jtype = atom_types[j]
-        #                parameters = ff.bond_increments(itype, jtype)[3]
-        #                q += float(parameters['deltaij'])
-        #            charges.append(q)
-        #            total_q += q
-        #        if abs(total_q) > 0.0001:
-        #            logger.warning(f'Total charge is not zero: {total_q:.4f}')
-        #            logger.info(
-        #                'Charges from increments and charges:\n' +
-        #                pprint.pformat(charges)
-        #            )
-        #        else:
-        #            logger.debug(
-        #                'Charges from increments:\n' + pprint.pformat(charges)
-        #            )
-
-        #        key = f'charges_{ffname}'
-        #        if key not in configuration.atoms:
-        #            configuration.atoms.add_attribute(key, coltype='float')
-        #        charge_column = configuration.atoms.get_column(key)
-        #        charge_column[0:] = charges
-        #        logger.debug(f"Set column '{key}' to the charges")
-
         # Get the energy expression.
 
         eex = seamm.EnergyExpression(configuration=configuration, atomtyping_engine=atomtyping_engine, style='LAMMPS').eex
