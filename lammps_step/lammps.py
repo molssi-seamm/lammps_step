1c55ce2e8743e9cdc38f2b33143beeb250358c55
Paul Saxe <paul@thesaxes.net>
2023-02-06 06:25:38 -0500

    Added support for extended OPLS functional forms.

--- lammps_step/lammps.py
+++ lammps_step/lammps.py
@@ -52,15 +52,18 @@ bond_style = {
 angle_style = {
     "quadratic_angle": "harmonic",
     "quartic_angle": "class2",
+    "simple_fourier_angle": "fourier/simple",
 }
 
 dihedral_style = {
     "torsion_1": "harmonic",
     "torsion_3": "class2",
+    "torsion_opls": "opls",
 }
 
 improper_style = {
     "wilson_out_of_plane": "class2",
+    "improper_opls": "cvff",
 }
 
 
@@ -586,6 +589,7 @@ class LAMMPS(seamm.Node):
             "*.restart.*",
             "*.dump.*",
             "*.log",
+            "*.dat",
             "log.cite",
         ]  # yapf: disable
 
@@ -621,7 +625,13 @@ class LAMMPS(seamm.Node):
             cmd = [str(lmp_serial)]
         cmd.extend(["-in", files["input"]["filename"]])
 
-        result = local.run(cmd=cmd, files=tmpdict, return_files=return_files)
+        result = local.run(
+            cmd=cmd,
+            files=tmpdict,
+            return_files=return_files,
+            in_situ=True,
+            directory=self.directory,
+        )
 
         if result is None:
             self.logger.error("There was an error running LAMMPS")
@@ -645,7 +655,7 @@ class LAMMPS(seamm.Node):
 
         if result["stderr"] != "":
             self.logger.warning("stderr:\n" + result["stderr"])
-            f = os.path.join(self.directory, "sstderr.txt")
+            f = os.path.join(self.directory, "stderr.txt")
             with open(f, mode="w") as fd:
                 fd.write(result["stderr"])
 
@@ -956,6 +966,9 @@ class LAMMPS(seamm.Node):
                         f"{counter:6d} {function} {values['Theta0']} "
                         f"{values['K2']} {values['K3']} {values['K4']}"
                     )
+                elif form == "simple_fourier_angle":
+                    function = "fourier/simple" if use_hybrid else ""
+                    line = f"{counter:6d} {function} {values['K']} -1 {values['n']}"
                 line += (
                     f" # {types[0]} {types[1]} {types[2]}"
                     f" --> {real_types[0]} {real_types[1]} {real_types[2]}"
@@ -1104,6 +1117,15 @@ class LAMMPS(seamm.Node):
                         f"{values['V1']} {values['Phi0_1']} "
                         f"{values['V2']} {values['Phi0_2']} "
                         f"{values['V3']} {values['Phi0_3']} "
+                    )
+                elif form == "torsion_opls":
+                    function = "opls" if use_hybrid else ""
+                    line = (
+                        f"{counter:6d} {function} "
+                        f"{values['V1']} "
+                        f"{values['V2']} "
+                        f"{values['V3']} "
+                        f"{values['V4']} "
                     )
                 line += (
                     f" # {types[0]}-{types[1]}-{types[2]}-{types[3]} "
@@ -1356,13 +1378,24 @@ class LAMMPS(seamm.Node):
             lines.append("")
             lines.append("Impropers")
             lines.append("")
+
+            # Need forms to reorder impropers....
+            form = {i: f[0] for i, f in enumerate(eex["oop parameters"], start=1)}
+
             for counter, tmp in zip(range(1, eex["n_oops"] + 1), eex["oops"]):
                 i, j, k, l, index = tmp
-                lines.append(
-                    "{:6d} {:6d} {:6d} {:6d} {:6d} {:6d}".format(
-                        counter, index, i, j, k, l
+                if form[index] == "improper_opls":
+                    lines.append(
+                        "{:6d} {:6d} {:6d} {:6d} {:6d} {:6d}".format(
+                            counter, index, j, i, k, l
+                        )
                     )
-                )
+                else:
+                    lines.append(
+                        "{:6d} {:6d} {:6d} {:6d} {:6d} {:6d}".format(
+                            counter, index, i, j, k, l
+                        )
+                    )
 
             lines.append("")
             lines.append("Improper Coeffs")
@@ -1371,19 +1404,30 @@ class LAMMPS(seamm.Node):
                 range(1, eex["n_oop_types"] + 1), eex["oop parameters"]
             ):
                 form, values, types, parameters_type, real_types = parameters
-                lines.append(
-                    "{:6d} {} {}".format(counter, values["K"], values["Chi0"])
-                    + " # {}-{}-{}-{} --> {}-{}-{}-{}".format(
-                        types[0],
-                        types[1],
-                        types[2],
-                        types[3],
-                        real_types[0],
-                        real_types[1],
-                        real_types[2],
-                        real_types[3],
+                if form == "wilson_out_of_plane":
+                    lines.append(
+                        "{:6d} {} {}".format(counter, values["K"], values["Chi0"])
+                        + " # {}-{}-{}-{} --> {}-{}-{}-{}".format(
+                            types[0],
+                            types[1],
+                            types[2],
+                            types[3],
+                            real_types[0],
+                            real_types[1],
+                            real_types[2],
+                            real_types[3],
+                        )
                     )
-                )
+                elif form == "improper_opls":
+                    # divide by two because OPLS uses V2/2 and CVS V.
+                    lines.append(
+                        f"{counter:6d} {float(values['V2'])/2:.4f} -1 2 "
+                        f"# {types[0]}-{types[1]}-{types[2]}-{types[3]} --> "
+                        f"{real_types[0]}-{real_types[1]}-{real_types[2]}-"
+                        f"{real_types[3]}"
+                    )
+                else:
+                    raise RuntimeError(f"Can't handle oop form '{form}'")
 
             # angle-angle
             if "n_angle-angle_types" in eex:
