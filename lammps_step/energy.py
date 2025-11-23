Fixed bug ... gradients in the db are stored as kJ/mol/Å, not kcal/mol/Å

lammps_step/energy.py
@@ -11,6 +11,7 @@ import pkg_resources
 import textwrap
 import traceback
 
+import numpy as np
 import pandas
 from tabulate import tabulate
 
@@ -354,8 +355,10 @@ class Energy(seamm.Node):
             if xyz is not None:
                 configuration.atoms.set_coordinates(xyz, fractionals=fractional)
             if gradients is not None:
-                # LAMMPS only has Cartesian gradients
-                configuration.atoms.set_gradients(gradients, fractionals=False)
+                # LAMMPS only has Cartesian gradients, in kcal/mol/Å
+                factor = Q_("kcal/mol/Å").m_as("kJ/mol/Å")
+                tmp = factor * np.array(gradients)
+                configuration.atoms.set_gradients(tmp, fractionals=False)
             if velocities is not None:
                 # LAMMPS only has Cartesian velocities
                 configuration.atoms.set_velocities(velocities, fractionals=False)